8857b555f21e7b886b123e913c5424fb6ec0928d
Ajit Padhi <v-padhiajit@microsoft.com>
2025-09-17 22:20:20 +0530

    updated sh file

--- infra/scripts/fabric/provision_fabric_items.sh
+++ infra/scripts/fabric/provision_fabric_items.sh
@@ -100,28 +100,28 @@ fabricCapacityName=""
 fabricWorkspaceName=""
 
 # Parse command line arguments
-while [[ $# -gt 0 ]]; do
-    case $1 in
-        -c|--capacity-name)
-            fabricCapacityName="$2"
-            shift 2
-            ;;
-        -w|--workspace-name)
-            fabricWorkspaceName="$2"
-            shift 2
-            ;;
-        -h|--help)
-            show_usage
-            exit 0
-            ;;
-        *)
-            print_error "❌ Unknown option: $1"
-            echo ""
-            show_usage
-            exit 1
-            ;;
-    esac
-done
+# while [[ $# -gt 0 ]]; do
+#     case $1 in
+#         -c|--capacity-name)
+#             fabricCapacityName="$2"
+#             shift 2
+#             ;;
+#         -w|--workspace-name)
+#             fabricWorkspaceName="$2"
+#             shift 2
+#             ;;
+#         -h|--help)
+#             show_usage
+#             exit 0
+#             ;;
+#         *)
+#             print_error "❌ Unknown option: $1"
+#             echo ""
+#             show_usage
+#             exit 1
+#             ;;
+#     esac
+# done
 
 # Variables
 baseUrl="$3"

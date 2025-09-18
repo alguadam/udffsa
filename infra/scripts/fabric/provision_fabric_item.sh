f45c367df2b9b0468ce954f996a3b6a7acd6acab
Ajit Padhi <v-padhiajit@microsoft.com>
2025-09-18 10:41:59 +0530

    updated sh

--- infra/scripts/fabric/provision_fabric_item.sh
+++ infra/scripts/fabric/provision_fabric_item.sh
@@ -10,12 +10,12 @@ requirementFile="requirements.txt"
 requirementFileUrl=${baseUrl}"infra/scripts/fabric/requirements.txt"
 
 echo "Downloading Python scripts..."
-wget -O "create_fabric_items.py" ${baseUrl}"infra/scripts/fabric/create_fabric_items.py"
-wget -O "fabric_api.py" ${baseUrl}"infra/scripts/fabric/fabric_api.py"
-wget -O "powerbi_api.py" ${baseUrl}"infra/scripts/fabric/powerbi_api.py"
+curl --output "create_fabric_items.py" ${baseUrl}"infra/scripts/fabric/create_fabric_items.py"
+curl --output "fabric_api.py" ${baseUrl}"infra/scripts/fabric/fabric_api.py"
+curl --output "powerbi_api.py" ${baseUrl}"infra/scripts/fabric/powerbi_api.py"
 
 # Download the requirement file
-wget -O "$requirementFile" "$requirementFileUrl"
+curl --output "$requirementFile" "$requirementFileUrl"
 
 # Check if workspace name is provided, otherwise use environment variable
 if [[ -z "$fabricWorkspaceName" ]]; then
